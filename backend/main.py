b121d043037d8d5233d7d08a86f0b4173c3a485d
Aditya <adityasorathiya.4112@gmail.com>
2026-06-06 17:18:47 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,39 @@
 from fastapi import FastAPI
 import subprocess
 import os
+import requests
+import zipfile
+import io
+
 
 app = FastAPI()
-
 
 @app.post("/deploy")
 def deploy(project_name: str, repo_url: str):
 
     os.makedirs("/app/projects", exist_ok=True)
 
-    path = f"/app/projects/{project_name}"
+    # Convert:
+    # https://github.com/user/repo.git
+    # ->
+    # https://github.com/user/repo/archive/refs/heads/main.zip
 
-    result = subprocess.run(
-        ["git", "clone", repo_url, path],
-        capture_output=True,
-        text=True
-    )
+    repo_url = repo_url.removesuffix(".git")
+    zip_url = f"{repo_url}/archive/refs/heads/main.zip"
+
+    project_path = f"/app/projects/{project_name}"
+
+    response = requests.get(zip_url)
+
+    if response.status_code != 200:
+        return {"error": "Could not download repository"}
+
+    with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+        z.extractall(project_path)
 
     return {
-        "returncode": result.returncode,
-        "stdout": result.stdout,
-        "stderr": result.stderr
+        "success": True,
+        "url": f"https://{project_name}.devploy.run.place"
     }
 
 
