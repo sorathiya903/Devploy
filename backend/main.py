b0f3e5b7037713ead4bb44f09089575ecf82a028
Aditya <adityasorathiya.4112@gmail.com>
2026-06-06 17:15:30 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,19 +4,26 @@ import os
 
 app = FastAPI()
 
+
 @app.post("/deploy")
 def deploy(project_name: str, repo_url: str):
 
+    os.makedirs("/app/projects", exist_ok=True)
+
     path = f"/app/projects/{project_name}"
 
-    subprocess.run(
+    result = subprocess.run(
         ["git", "clone", repo_url, path],
-        check=True
+        capture_output=True,
+        text=True
     )
 
     return {
-        "url": f"https://{project_name}.devploy.run.place"
+        "returncode": result.returncode,
+        "stdout": result.stdout,
+        "stderr": result.stderr
     }
+
 
 @app.get("/")
 def welcome():
